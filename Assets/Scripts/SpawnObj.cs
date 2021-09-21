3acc2c802886fc42f76e90d554959a2f3d55674c
PacoVelazquezP <frvelazquezpa@ucq.edu.mx>
2021-09-21 16:11:12 -0500

    Guide lines

---
+++ Assets/Scripts/SpawnObj.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObj : MonoBehaviour
+{
+    [SerializeField] private float nextSpawn;
+    [SerializeField] private float spawnRate;
+    public GameObject trailRenderObj;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ToSpawnObj();
+    }
+
+    public void ToSpawnObj()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            Instantiate(trailRenderObj);
+            Debug.Log("spawn");
+        }
+    }
+}
---
+++ Assets/Scripts/TrailRender.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailRender : MonoBehaviour
+{
+   
+    public Transform[] Targets;
+    Transform NextPos;
+    public int nextPosIndex;
+    public float speed;
+    Vector3 a;
+    void Start()
+    {
+        NextPos = Targets[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPosition();
+    }
+
+    public void FollowPosition()
+    {
+        if(transform.position == NextPos.position)
+        {
+            nextPosIndex++;
+            if (nextPosIndex >= Targets.Length)
+            {
+                nextPosIndex = 0;
+            }
+            NextPos = Targets[nextPosIndex];
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, NextPos.position, speed * Time.deltaTime);
+        }
+    }
+
+  
+}
