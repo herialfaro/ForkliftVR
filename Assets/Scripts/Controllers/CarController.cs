19ef7ac140f116e97ca296a32b5c4a47202e62c2
lalosavior13 <lalosavior13@gmail.com>
2021-10-05 11:35:52 -0500

    Modified "CarController", Deleted "PlayerInput"

--- Assets/Scripts/Controllers/CarController.cs
+++ Assets/Scripts/Controllers/CarController.cs
@@ -4,26 +4,87 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
 
-    private PlayerInput inputManager;
-    public List<WheelCollider> throttleWheels;
-    public List<WheelCollider> steeringWheels;
-    public float strengthCoefficient = 200000f;
-    public float maxTurn = 20f;
+    private float horixontalInput;
+    private float vertivalInput;
+    private float currentBreakForce;
+    private bool isBreaking;
+    private float currentSteerAngle;
 
-    void Start()
-    {
-        inputManager = GetComponent<PlayerInput>();
-    }
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerAngle;
 
+
+    [SerializeField] private WheelCollider frontLeftWheel;
+    [SerializeField] private WheelCollider frontRightWheel;
+    [SerializeField] private WheelCollider rearLeftWheel;
+    [SerializeField] private WheelCollider rearRightWheel;
+
+    [SerializeField] private Transform frontLeftTransform;
+    [SerializeField] private Transform frontRightTransform;
+    [SerializeField] private Transform rearLeftTransform;
+    [SerializeField] private Transform rearRightTransform;
 
     private void FixedUpdate()
     {
-        foreach (WheelCollider wheel in throttleWheels)
-        {
-            wheel.motorTorque = strengthCoefficient * Time.deltaTime * inputManager.Acceleration;
-            wheel.wheelDampingRate = inputManager.wheelDampening;
-        }
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
     }
 
+    private void HandleMotor()
+    {
+        frontLeftWheel.motorTorque = vertivalInput * motorForce;
+        frontRightWheel.motorTorque = vertivalInput * motorForce;
+        currentBreakForce = isBreaking ? breakForce : 0f;
+        
+        if (isBreaking)
+        {
+            ApplyBreaking();
+        }
+
+    }
+
+    private void ApplyBreaking()
+    {
+        frontRightWheel.brakeTorque = currentBreakForce;
+        frontLeftWheel.brakeTorque = currentBreakForce;
+        rearRightWheel.brakeTorque = currentBreakForce;
+        rearLeftWheel.brakeTorque = currentBreakForce;
+    }
+
+    private void GetInput()
+    {
+        horixontalInput = Input.GetAxis(HORIZONTAL);
+        vertivalInput = Input.GetAxis(VERTICAL);
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * horixontalInput;
+        frontLeftWheel.steerAngle = currentSteerAngle;
+        frontRightWheel.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheel, frontLeftTransform);
+        UpdateSingleWheel(frontRightWheel, frontRightTransform);
+        UpdateSingleWheel(rearLeftWheel, rearLeftTransform);
+        UpdateSingleWheel(rearRightWheel, rearRightTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
 }
