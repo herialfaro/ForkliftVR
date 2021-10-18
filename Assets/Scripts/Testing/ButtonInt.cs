9a6c4f64b739b7288ed8a4e669c51ee9c6740db7
PacoVelazquezP <frvelazquezpa@ucq.edu.mx>
2021-10-18 11:33:19 -0500

    Get Down of the forklift

--- Assets/Scripts/Testing/ButtonInt.cs
+++ Assets/Scripts/Testing/ButtonInt.cs
@@ -7,28 +7,28 @@ using Valve.VR;
 
     public class ButtonInt : MonoBehaviour
     {
-    public HoverButton hoverButton;
-    public CarController carController;
+        public HoverButton hoverButton;
+        public CarController carController;
 
-    private void Start()
-    {
-        hoverButton.onButtonDown.AddListener(OnButtonDown);
-    }
+        private void Start()
+        {
+            this.hoverButton.onButtonDown.AddListener(OnButtonDown);
+        }
 
-    private void OnButtonDown(Hand hand)
-    {
-        StartCoroutine(DoMove());
-    }
+        private void OnButtonDown(Hand hand)
+        {
+            StartCoroutine(DoMove());
+        }
 
 
-    private IEnumerator DoMove()
-    {
-        //movePosition = FindObjectOfType<MovePosition>();
-        this.carController.ForMove();
+        private IEnumerator DoMove()
+        {
+            //movePosition = FindObjectOfType<MovePosition>();
+            this.carController.ForMove();
 
-        // Rigidbody rigidbody = moving.GetComponent<Rigidbody>();
+            // Rigidbody rigidbody = moving.GetComponent<Rigidbody>();
 
-        yield return null;
-    }
+            yield return null;
+        }
 }
 
--- Assets/Scripts/Testing/TransformPosition.cs
+++ Assets/Scripts/Testing/TransformPosition.cs
@@ -23,4 +23,6 @@ public class TransformPosition : MonoBehaviour
             }
         
     }
+
+
 }
