c765ace35093e26b0970ee51466878b1d3688642
gil-dominguez <gihernandezdo@ucq.edu.mx>
2021-11-30 11:08:24 -0600

    Minorchanges on tutorial

---
+++ Assets/Scripts/Testing/CargoParent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CargoParent : MonoBehaviour
+{
+    public Transform pointOfParent;
+    public Transform cargoTransform;
+    public string _tag;
+    private bool isBeingMoved = false;
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag(_tag))
+        {
+            isBeingMoved = true;
+            cargoTransform.SetParent(pointOfParent);
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag(_tag))
+        {
+            isBeingMoved = false;
+            cargoTransform.SetParent(null);
+        }
+    }
+
+    private void Update()
+    {
+        if(isBeingMoved)
+        {
+            transform.position = pointOfParent.position;
+        }
+        
+    }
+}
